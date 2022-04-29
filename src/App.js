73058af94e88a1984152f25da52f42041c152836
Cody-Wise <cody@sharptackmedia.com>
2022-04-29 10:57:59 -0700

    Playing with boostrap and buttons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import './App.css';
 import Header from './Header/Header.js';
 import Footer from './Footer/Footer.js';
 import Main from './Main/Main.js';
 import { zodiacs } from './data.js';
+// import './custom.scss';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
-    <div className="App">
-      <Header
-        name='Cody'/>
+    <div className='App'>
+      <Header name='Cody' />
+      <Button bsClass='btn-custom'>Primary</Button>
 
-      <Main
-        zodiacs = {zodiacs}/>
-      
-      <Footer
-        year='2022'
-      />
+      <Main zodiacs={zodiacs} />
+
+      <Footer year='2022' />
       {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
     </div>
   );
